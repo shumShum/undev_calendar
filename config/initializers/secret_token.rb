ae73999cfa17d6e6701190ef32bb3fae05745c97
Геннадий Шумилкин <shumilkin.gena@yandex.ru>
2013-06-06 13:36:43 +0400

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UndevCalendar::Application.config.secret_token = 'dd79028d541b77f98f167fe5b5aaa18c3d7cf1546dc53767adf967ddcdae5011664d28b95c54b21c41e922bb8cf9c39842b5ee62a298090878a917e4ef90519f'
