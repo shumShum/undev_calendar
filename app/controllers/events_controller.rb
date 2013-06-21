a3654f1fd7d4421fc8b4878f9658f97543621bfb
Геннадий Шумилкин <shumilkin.gena@yandex.ru>
2013-06-21 18:02:12 +0400

    week cal work

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -26,13 +26,23 @@ class EventsController < ApplicationController
 	]
 
 	def index
+		time_option = params[:time_option].present? ? params[:time_option] : 'month'
 		params[:year].present? ? @year = params[:year].to_i : @year = Date.today.year
-		params[:month].present? ? @month = params[:month].to_i : @month = Date.today.month
-		params[:day].present? ? @day = params[:day].to_i : @day = Date.today.day
+		unless time_option == 'week'
+			params[:month].present? ? @month = params[:month].to_i : @month = Date.today.month
+			params[:day].present? ? @day = params[:day].to_i : @day = Date.today.day
+			@week = @month * 4
+		else
+			@week = params[:month].to_i if params[:month].present?
+			pre_date = Date.new(@year) + (@week.to_i-1).week
+			wday = pre_date.wday == 0 ? 7 : pre_date.wday
+			@begin_of_week = pre_date - (wday - 1).day
+			@week.present? ? @month = @begin_of_week.month : @month = Date.today.month
+			@day = @begin_of_week.day
+		end
 		@begin_month_date = Date.new(@year, @month)
 		@date = Date.new(@year, @month, @day)
-		time_option = params[:time_option].present? ? params[:time_option] : 'month'
-
+		
 			if time_option == 'month'
 				events = Event.arel_table
 				@events = Event.where(events[:is_repeat].eq(true).or(events[:date].in(@begin_month_date-1.week..@begin_month_date+1.month+1.week))).all
@@ -43,6 +53,12 @@ class EventsController < ApplicationController
 				events = Event.arel_table
 				@events = Event.where(events[:is_repeat].eq(true).or(events[:date].eq(@date))).all
 				actual_events = @events.select{|x| x.date.to_date == @date}
+			end
+
+			if time_option == 'week'
+				events = Event.arel_table
+				@events = Event.where(events[:is_repeat].eq(true).or(events[:date].in(@date..@date + 1.week))).all
+				actual_events = @events.select{|x| (x.date >= @date) && (x.date <= @date + 1.week)}
 			end
 
 				
@@ -72,6 +88,14 @@ class EventsController < ApplicationController
 										repeat_events.push rep_event
 									end
 								end
+								if (time_option == 'week') && (@date + Date::ABBR_DAYNAMES.index(d).day > e.date.to_date)
+									rep_event = Event.new(e.as_json.reject{|key| key == "updated_at" || key == "created_at"})
+									rep_event[:id] = e.id
+									add_days = Date::ABBR_DAYNAMES.index(d)
+									add_days = add_days == 0 ? 6 : add_days - 1
+									rep_event[:date] = @date + add_days
+									repeat_events.push rep_event
+								end
 							end
 							if e.repeat_type == "month"
 								if time_option == 'month' 
@@ -90,6 +114,16 @@ class EventsController < ApplicationController
 										rep_event = Event.new(e.as_json.reject{|key| key == "updated_at" || key == "created_at"})
 										rep_event[:id] = e.id
 										rep_event[:date] = @date
+										repeat_events.push rep_event
+									end
+								end
+								if (time_option == 'week')
+									days = (@date..@date + 1.week).to_a.map{|x| x.day}
+									if days.index(d.to_i).present?
+										dm = days.index(d.to_i)
+										rep_event = Event.new(e.as_json.reject{|key| key == "updated_at" || key == "created_at"})
+										rep_event[:id] = e.id
+										rep_event[:date] = @date + dm.day
 										repeat_events.push rep_event
 									end
 								end
